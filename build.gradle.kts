3cd60214d7070ded72df3c623201e6f8d835a578
nikiton <fefelovnu@gmail.com>
2024-04-27 09:53:41 +0700

    publish test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,35 @@
+import java.net.URI
+
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI(/* str = */ "https://maven.pkg.github.com/n1k1t0n/workflow-test")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        register("workflow-pub-test", MavenPublication::class) {
+            from(components["java"])
+            artifact(tasks.named("jar"))
+            groupId = "project.group"
+            artifactId = "project.name"
+            version = "project.version"
+        }
+    }
+}
 
 repositories {
     mavenCentral()
